e97b0c17a641338cd225c0b956d1682b9f545131
Aleksa Ljubisavljevic <aleksa.ljubisav@gmail.com>
2022-05-25 21:33:18 +0200

    Dodao build, kernel i kernel.asm u .gitignore

--- build/src/main.d
+++
@@ -1,8 +0,0 @@
-build/src/main.o: src/main.cpp src/../h/MemoryAllocator.hpp \
- src/../h/../lib/hw.h src/../h/print.hpp
-
-src/../h/MemoryAllocator.hpp:
-
-src/../h/../lib/hw.h:
-
-src/../h/print.hpp:
