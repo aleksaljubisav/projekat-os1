e6a026a3cb2bba7c102e0d3d5b8f6067381752db
Aleksa Ljubisavljevic <aleksa.ljubisav@gmail.com>
2022-05-28 12:56:55 +0200

    Valjda zavrseno pod 2) Prekidna rutina

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include "../h/MemoryAllocator.hpp"
-#include "../h/print.hpp"
 #include "../lib/console.h"
 #include "../lib/hw.h"
+#include "../h/riscv.hpp"
+#include "../h/print.hpp"
 
-int main() {
+int main(){
+    printString("ENTER\n");
+
+    Riscv::w_stvec((uint64) &Riscv::supervisorTrap);
+    __asm__ volatile("ecall");
+
+    printString("EXIT\n");
+
 
     return 0;
 }
---
+++ src/riscv.cpp
@@ -0,0 +1,19 @@
+//
+// Created by os on 5/27/22.
+//
+
+//#include "../h/riscv.hpp"
+//#include "../h/tcb.hpp"
+//#include "../lib/console.h"
+
+/*void Riscv::popSppSpie()
+{
+    __asm__ volatile("csrw sepc, ra"); // zbog ovoga nije smelo da bude inline!!!
+    __asm__ volatile("sret"); // sret ce da popuje ove dve stvari
+    // zelimo da se vratimo tamo gde ce ova funkcija i biti pozvana, na pocetak threadWrapper-a
+}
+
+void Riscv::handleSupervisorTrap()
+{
+
+}*/
